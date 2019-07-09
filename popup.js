fac856305ca6f35d9f027cab9b3833b4f8b01d1f
Amelie Oller <amelieoller@gmail.com>
2019-07-09 16:48:23 -0700

    Initial commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+let sgObject = document.getElementById('sg-objects');
+sgObject.focus();
+
+chrome.storage.sync.get('sg', function(data) {
+	sgObject.value = data.sg;
+});
+
+sgObject.addEventListener(
+	'input',
+	function(element) {
+		let newSgObject = element.target.value;
+		debugger;
+		chrome.storage.sync.set({ sg: newSgObject });
+	},
+	false
+);
+
+let submitButton = document.getElementById('submit-button');
+
+submitButton.onclick = function() {
+	var sgData = sgObject.value;
+
+	chrome.tabs.executeScript(
+		null,
+		{ code: 'var sgData = ' + sgData },
+		function() {
+			chrome.tabs.executeScript(null, { file: 'inject.js' }, function() {});
+		}
+	);
+};
